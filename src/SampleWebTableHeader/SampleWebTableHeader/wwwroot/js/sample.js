288cd71d80791e8dd743d09694c31ba61c1fbfb0
u-suke510 <usuke510@outlook.jp>
2021-12-10 13:59:06 +0900

    JSでのヘッダー固定処理追加 ＃IEで表示すると、固定がちらつく問題あり。

--- src/SampleWebTableHeader/SampleWebTableHeader/wwwroot/js/sample.js
+++ src/SampleWebTableHeader/SampleWebTableHeader/wwwroot/js/sample.js
@@ -1,1 +1,9 @@
-﻿
+﻿function ScrollTableFrame()
+{
+    var frame = $(this);
+
+    var headerRow = $(".sample-table.header-row");
+    headerRow.css({ top: frame.scrollTop(), left: 0 });
+    var headerCol = $(".sample-table.header-col");
+    headerCol.css({ top: 0, left: frame.scrollLeft() });
+}
